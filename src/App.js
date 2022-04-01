bf9260f135fec222135ebf181af0b7dd812c3781
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-04-01 09:08:49 +0530

    setup routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Navigation from "./layouts/Navigation";
 
 const App = () => {
   return (
-    <div>
-      <h1>This is my app</h1>
-    </div>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 };
 
